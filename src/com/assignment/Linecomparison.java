c9a1243d6f33e7dd2f015b8590dfbdc64227ecd0
RagulKanna <ragulragi619@gmail.com>
2021-09-01 13:56:09 +0530

    Added:Welcome message

---
+++ src/com/assignment/Linecomparison.java
@@ -0,0 +1,7 @@
+package com.assignment;
+
+public class Linecomparison {
+    public static void main(String[] args) {
+        System.out.println("\n   Welcome to Line Comparison Computation Program");
+    }
+}
