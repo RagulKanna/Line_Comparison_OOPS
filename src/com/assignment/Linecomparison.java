29c1ebfe091a41f53da358200e5c987c05adb37b
RagulKanna <ragulragi619@gmail.com>
2021-09-01 16:13:00 +0530

    Added:check the equality of two lines

--- src/com/assignment/Linecomparison.java
+++ src/com/assignment/Linecomparison.java
@@ -6,6 +6,25 @@ public class Linecomparison {
 
     Scanner sc=new Scanner(System.in);
     public static int x1,x2,y1,y2;
+    public static double length,flength,slength;
+
+    public void getinputcalculatelength()
+    {
+        System.out.println("\n\ngetting two line input:");
+        System.out.println("\n  for first line:");
+        cordinatesinput();
+        System.out.println("\n\nFor first Line");
+        lengthcalculation();
+        flength=length;
+        System.out.print("\n  the length of the first line for the values of (x1,x2) as ("+x1+","+x2+") & (y1,y2) as ("+y1+","+y2+") is "+flength);
+        System.out.println("\n  for second line:");
+        cordinatesinput();
+        System.out.println("\n\nFor second line");
+        lengthcalculation();
+        slength=length;
+        System.out.print("\n  the length of the second line for the values of (x1,x2) as ("+x1+","+x2+") & (y1,y2) as ("+y1+","+y2+") is "+slength);
+    }
+
 
     public void cordinatesinput()
     {
@@ -23,15 +42,29 @@ public class Linecomparison {
     public void lengthcalculation()
     {
         System.out.println("\n\nCalcuate Length of the line:");
-        double length= Math.sqrt( ( (Math.pow( ( x2-x1 ),2 ) ) + (Math.pow( ( y2-y1 ),2 ) ) ) );
-        System.out.println("\n  the length of the line for the values of (x1,x2) as ("+x1+","+x2+") & (y1,y2) as ("+y1+","+y2+") is "+length);
+        length= Math.sqrt( ( (Math.pow( ( x2-x1 ),2 ) ) + (Math.pow( ( y2-y1 ),2 ) ) ) );
+    }
+
+    public void checkequalityofline()
+    {
+        System.out.println("\n\nCheck the equality of line:");
+        Double line1=new Double(flength);
+        Double line2=new Double(slength);
+        double equal=line1.compareTo(line2);
+        if(equal>0.0)
+            System.out.println("The endpoint of 1st line is greater than endpoint of 2nd line");
+        else if(equal<0.0)
+            System.out.println("The endpoint of 1st line is lesser than endpoint of 2nd line");
+        else
+            System.out.println("The endpoint of 1st line is equal to endpoint of 2nd line");
     }
 
     public static void main(String[] args) {
         System.out.println("\n   Welcome to Line Comparison Computation Program");
 
         Linecomparison line= new Linecomparison();
-        line.cordinatesinput();
-        line.lengthcalculation();
+        line.getinputcalculatelength();
+        line.checkequalityofline();
     }
+
 }
